fbf3ea90fc423b724a9454034b3d4443316cdd17
韩强 <hanqiang@huadezhihui.com>
2025-07-04 17:02:02 +0800

    ✨ feat(openapi): default download

--- apps/server/tasky-server/src/lib/configure-open-api.ts
+++ apps/server/tasky-server/src/lib/configure-open-api.ts
@@ -1,6 +1,10 @@
 import type { AppOpenAPI } from './create-app.ts'
 
 export function configureOpenAPI(app: AppOpenAPI) {
+  app.use('/doc', (c, next) => {
+    // c.header('Content-Disposition', 'attachment; filename="open-api.json')
+    return next()
+  })
   app.doc('/doc', {
     openapi: '3.0.0',
     info: {
